816a38aff3879043972908a860f4af89bf93db83
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-04-24 22:07:11 -0700

    Refactored fish functions and added some vim plugins

--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -3,6 +3,7 @@ alias chrome 'google-chrome-unstable'
 alias e "$EDITOR"
 alias lsports 'netstat -pelnut'
 alias git 'hub'
+alias subl 'subl3'
 
 # Functions
 function fish_greeting
@@ -10,57 +11,38 @@ function fish_greeting
     echo
 end
 
-function l
+function l -d "Lists all files in a directory or reads a file"
     if test -f "$argv"
         less -N "$argv"
-	else
-		ls -CAF --color=auto $argv
-	end
+    else
+        ls -CAF --color=auto $argv
+    end
 end
 
-function ll
+function ll -d "Lists all files in a directory and prints their permissions and shit"
     l -l $argv
 end
 
-function lc
+function lc -d "Lists all files in a directory in a column"
     ll $argv | tail -n +2 | awk '{print $9}'
 end
 
 function mkcd
-	if test ! -d "$argv"
-		mkdir -p "$argv"
-	end
-	cd "$argv"
-end
-
-function myip
-	if test "$argv" = "--public"
-		wget http://ipinfo.io/ip -qO -
-	else if test "$argv" = "--help"
-		echo "myip [--public|--help]"
-		echo "myip     - Prints the internal IP address"
-		echo "--public - Prints the public IP of the access point"
-		echo "--help   - Prints this message"
-	else
-		# Thank you!: http://www.if-not-true-then-false.com/2010/linux-get-ip-address/
-		ifconfig | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}' | tail -n 1
-	end
-end
-
-function r
-	rm -rf $argv
-end
-
-function reboot-required
-    if test -f "/run/reboot-required"
-        echo "Reboot required!"
-    else
-        echo "You don't have to reboot!"
+    if test ! -d "$argv"
+        mkdir -p "$argv"
     end
+    cd "$argv"
 end
 
-function tree
+function my-public-ip -d "Gets the public ip using an external service"
+    wget http://ipinfo.io/ip -qO-
+end
+
+function r -d "An alias for rm -rf"
+    rm -rf "$argv"
+end
+
+function tree -d "Runs the tree command with color enabled and pipes it to less for paging"
    eval  (which tree) -C $argv | less
 end
-
 

c7b3597a08987c21bb06323ce96ae0331c30a6c5
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-05-27 14:34:16 -0700

    Suppress fish error when setting env variable that points to nonexistent path

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -4,9 +4,9 @@ set -gx LESS '-R'
 set -gx LESSOPEN '|pygmentize -g %s'
 set -gx LANG en_US.UTF-8
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
-set -gx USR_LOCAL /usr/local/bin /usr/local/sbin
-set -gx JAVA_HOME /usr/lib/jvm/default
-set -gx ANDROID_HOME /opt/android-sdk
-set -gx ANDROID $ANDROID_HOME/tools $ANDROID_HOME/platform-tools
-set -gx PATH $PATH $USR_LOCAL $ANDROID $JAVA_HOME/bin
+set -gx USR_LOCAL /usr/local/bin /usr/local/sbin ^ /dev/null
+set -gx JAVA_HOME /usr/lib/jvm/default ^ /dev/null
+set -gx ANDROID_HOME /opt/android-sdk ^ /dev/null
+set -gx ANDROID $ANDROID_HOME/tools $ANDROID_HOME/platform-tools ^ /dev/null
+set -gx PATH $PATH $USR_LOCAL $ANDROID $JAVA_HOME/bin ^ /dev/null
 

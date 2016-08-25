073ab53ec3d12cdc912ccf869e03024d90129382
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-08-25 09:41:41 -0700

    God damn it lol

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,7 +4,7 @@ source ~/.config/fish/functions.fish
 # Init stuff
 which keychain > /dev/null ^ /dev/null
 if test -z $DISPLAY
-    if test $status -ne 0
+    if test $status -eq 0
         # Start keychain quietly
         eval (keychain --eval --agents ssh -Q --quiet --nogui $HOME/.ssh/id_rsa)
     end

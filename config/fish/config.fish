b5bd6b682e1d2c4320270e85e576e9639a6f4829
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-08-25 20:15:21 -0700

    Did several things lol

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,7 @@ source ~/.config/fish/variables.fish
 source ~/.config/fish/functions.fish
 
 # Init stuff
-which keychain > /dev/null ^ /dev/null
-if test -z $DISPLAY
+if test -z $DISPLAY; and exists keychain
     if test $status -eq 0
         # Start keychain quietly
         eval (keychain --eval --agents ssh -Q --quiet --nogui $HOME/.ssh/id_rsa)
--- config/fish/functions.fish
+++ config/fish/functions.fish
@@ -1,54 +1,27 @@
-# Aliases
-alias e "$EDITOR"
-alias g 'git'
-
-# Conditional Aliases
-if which 'apm-beta' ^ /dev/null > /dev/null
-    alias apm 'apm-beta'
-end
-
-if which 'atom-beta' ^ /dev/null > /dev/null
-    alias atom 'atom-beta'
-end
-
-if which 'google-chrome-unstable' ^ /dev/null > /dev/null
-    alias chrome 'google-chrome-unstable'
-end
-
-if which 'hub' ^ /dev/null > /dev/null
-    alias git 'hub'
-    alias g 'hub'
-end
-
-if which 'netstat' ^ /dev/null > /dev/null
-    alias lsports 'netstat -pelnut'
-end
-
-if which 'npm' ^ /dev/null > /dev/null
-    alias npmls 'npm ls --depth=0'
-end
-
-if which 'subl3' ^ /dev/null > /dev/null
-    alias subl 'subl3'
-end
-
-alias pdflatex 'pdflatex -interaction=nonstopmode -shell-escape'
-
 # Functions
 function fish_greeting
     fortune 50% myfortunes 30% off 20% ascii-art | cowsay -n
     echo
 end
 
+function exists -d 'Silent wrapper over "which". Returns the status of the command'
+    if test (count $argv) -gt 0
+        which $argv[1] > /dev/null ^ /dev/null
+    else
+        echo 'You need to specify a command.'
+        return -1
+    end
+end
+
 function l -d "Lists all files in a directory or reads a file"
     if test -f "$argv"
         less -N "$argv"
     else
-	if test (uname) = "Darwin"
-	    ls -CAF $argv
-	else
+    if test (uname) = "Darwin"
+        ls -CAF $argv
+    else
             ls -CAF --color=auto $argv
-	end
+    end
     end
 end
 
@@ -77,5 +50,44 @@ end
 
 function tree -d "Runs the tree command with color enabled and pipes it to less for paging"
    eval  (which tree) -C $argv | less
+end
+
+# Aliases
+alias e "$EDITOR"
+alias g 'git'
+
+
+# Conditional Aliases
+if exists apm-beta
+    alias apm 'apm-beta'
+end
+
+if exists atom-beta
+    alias atom 'atom-beta'
+end
+
+if exists google-chrome-unstable
+    alias chrome 'google-chrome-unstable'
+end
+
+if exists hub
+    alias git 'hub'
+    alias g 'hub'
+end
+
+if exists netstat
+    alias lsports 'netstat -pelnut'
+end
+
+if exists npm
+    alias npmls 'npm ls --depth=0'
+end
+
+if exists subl3
+    alias subl 'subl3'
+end
+
+if exists pdflatex
+    alias pdflatex 'pdflatex -interaction=nonstopmode -shell-escape'
 end
 

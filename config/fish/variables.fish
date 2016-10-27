c4797ba2d311a76fac8ec96167a800f6edb47d22
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-10-26 23:45:17 -0700

    Added color variables for fish shell

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -60,3 +60,29 @@ if test (uname) = Linux
     set -gx PATH $PATH $USR_LOCAL $ANDROID $JAVA_HOME/bin ^ /dev/null
 end
 
+# Color settings
+set -gx fish_color_autosuggestion 8a8a8a
+set -gx fish_color_command 005fd7
+set -gx fish_color_comment ff8700
+set -gx fish_color_cwd green
+set -gx fish_color_cwd_root red
+set -gx fish_color_end 00FF00
+set -gx fish_color_error FF0000
+set -gx fish_color_escape cyan
+set -gx fish_color_history_current cyan
+set -gx fish_color_host normal
+set -gx fish_color_match cyan
+set -gx fish_color_normal normal
+set -gx fish_color_operator cyan
+set -gx fish_color_param 00afff
+set -gx fish_color_quote 8700ff
+set -gx fish_color_redirection normal
+set -gx fish_color_search_match \x2d\x2dbackground\x3dpurple
+set -gx fish_color_selection \x2d\x2dbackground\x3dpurple
+set -gx fish_color_status red
+set -gx fish_color_user green
+set -gx fish_color_valid_path \x2d\x2dunderline
+set -gx fish_pager_color_completion normal
+set -gx fish_pager_color_description 555\x1eyellow
+set -gx fish_pager_color_prefix cyan
+set -gx fish_pager_color_progress cyan

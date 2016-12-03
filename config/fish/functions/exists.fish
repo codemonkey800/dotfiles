ba7b5068765a352b8bf0e30dc58793c66c11cb17
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-12-03 05:17:29 -0800

    Switched exists.fish command to type for more command support

--- config/fish/functions/exists.fish
+++ config/fish/functions/exists.fish
@@ -1,6 +1,6 @@
-function exists -d 'Silent wrapper over "which". Returns the status of the command'
+function exists -d 'Silent wrapper over "type". Returns the status of the command'
     if test (count $argv) -gt 0
-        which $argv[1] > /dev/null ^ /dev/null
+        type $argv[1] > /dev/null ^ /dev/null
     else
         echo 'You need to specify a command.'
         return -1

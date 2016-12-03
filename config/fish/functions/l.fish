4796c292b0f2ed2dc3780d6ea6f442b14b7f0cce
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-12-03 01:45:58 -0800

    Rewrote l.fish script because it was shit before

--- config/fish/functions/l.fish
+++ config/fish/functions/l.fish
@@ -1,18 +1,42 @@
 function l -d 'Lists all files in a directory or reads a file'
-    if test (count $argv) -gt 0; and test -f $argv[-1]
-        set -l i (contains -i -- -p $argv)
-        set -l j (contains -i -- --pager $argv)
-        if test \( -n $i \) -a \( $i -gt 0 \); or test \( -n $j \) -a \( $j -gt 0 \)
-            less -N $argv[-1]
+    set pager false
+    set args
+    set files
+
+    for i in (seq (count $argv))
+        set -l arg $argv[$i]
+        switch $arg
+            case -p --pager
+                set pager true
+            case '--*' '-*'
+                set args $args $arg
+            case '*'
+                if test -e $arg
+                    set files $files $arg
+                end
+        end
+    end
+
+    if test (count $files) -eq 1; and test -f $files[1]
+        if eval $pager
+            nvim -c 'runtime! macros/less.vim' $files[1]
         else
-            pygmentize -g $argv[-1]
+            pygmentize -g $files[1]
         end
     else
-        if test (uname) = 'Darwin'
-            ls -CAF $argv
-        else
-            ls -CAF --color=auto $argv
+        set -l cmd 'ls -CAF'
+
+        if test (uname) = 'Linux'
+            set cmd "$cmd --color=auto"
         end
+
+        set cmd "$cmd $args $files"
+
+        if eval $pager
+            set cmd "$cmd | $PAGER"
+        end
+
+        eval $cmd
     end
 end
 

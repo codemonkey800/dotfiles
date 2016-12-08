86320a9bacbf2b370ed5f569c4065ef0bb79974f
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2016-12-07 22:17:52 -0800

    Resorted aliases, changed exists.fish into alias, added completions for new update script, changed variables to set -gx

--- config/fish/functions/exists.fish
+++
@@ -1,9 +0,0 @@
-function exists -d 'Silent wrapper over "type". Returns the status of the command'
-    if test (count $argv) -gt 0
-        type $argv[1] > /dev/null ^ /dev/null
-    else
-        echo 'You need to specify a command.'
-        return -1
-    end
-end
-
--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -1,5 +1,5 @@
 # Dotfiles path
-setenv DOTFILES (
+set -gx DOTFILES (
     set dir (status -f)
     if test -L $dir
         set dir (readlink $dir)
@@ -11,39 +11,49 @@ setenv DOTFILES (
 begin
     set -l dir $DOTFILES/config/fish/functions
     if not contains $dir -- $fish_function_path
-        setenv fish_function_path $fish_function_path $dir
+        set -gx fish_function_path $fish_function_path $dir
     end
 end
 
 # Less stuff
-setenv LESS '-R'
-setenv LESSOPEN '|pygmentize -g %s'
+set -gx LESS '-R'
+set -gx LESSOPEN '|pygmentize -g %s'
 
 # Development environment
-setenv DIRENV_LOG_FORMAT
-setenv EDITOR (which nvim)
-setenv VIRTUAL_ENV_DISABLE_PROMPT 1
+set -gx DIRENV_LOG_FORMAT
+set -gx EDITOR (which nvim)
+# set -gx EDITOR (
+#     if type -q nvr
+#         which nvr
+#     else if type -q nvim
+#         which nvim
+#     else
+#         which vi
+#     end
+# )
+set -gx NVIM_LISTEN_ADDRESS /tmp/nvim-(whoami).socket
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # GPG stuff
-setenv ENCRYPTION_KEY A1CE3226F4FDEFF5
-setenv GPG_TTY (tty)
-setenv SIGNING_KEY 3FCC32880193C153
+set -gx ENCRYPTION_KEY A1CE3226F4FDEFF5
+set -gx GPG_TTY (tty)
+set -gx SIGNING_KEY 3FCC32880193C153
 
 # Locale stuff
-setenv LANG en_US.UTF-8
+set -gx LANG en_US.UTF-8
 
 # Pagerstuff
-setenv MANPAGER "$EDITOR -c 'setf man' -c 'runtime! macros/less.vim' -"
-setenv PAGER "$EDITOR -c 'runtime! macros/less.vim' -c AnsiEsc -"
+set -gx MANPAGER "$EDITOR -c 'setf man' -c 'runtime! macros/less.vim' -"
+set -gx PAGER "$EDITOR -c 'runtime! macros/less.vim' -c AnsiEsc -"
 
 # PATH stuff
-mkdir -p ~/src/go;and setenv GOPATH ~/src/go
+mkdir -p ~/src/go;and set -gx GOPATH ~/src/go
 
 function __add_to_path
     for arg in $argv
         if not contains $arg -- $PATH
             mkdir -p (dirname $arg)
-            setenv PATH $PATH $arg
+            set -gx PATH $PATH $arg
         end
     end
 end
@@ -52,36 +62,36 @@ __add_to_path $GOPATH/bin
 __add_to_path (find $DOTFILES/bin -type d)
 
 # Fzf stuff
-if exists fzf
-    if exists tmux
-        setenv FZF_TMUX 1
+if type -q fzf
+    if type -q tmux
+        set -gx FZF_TMUX 1
     end
 end
 
 # Color settings
-setenv fish_color_autosuggestion 8a8a8a
-setenv fish_color_command 005fd7
-setenv fish_color_comment ff8700
-setenv fish_color_cwd green
-setenv fish_color_cwd_root red
-setenv fish_color_end 00FF00
-setenv fish_color_error FF0000
-setenv fish_color_escape cyan
-setenv fish_color_history_current cyan
-setenv fish_color_host normal
-setenv fish_color_match cyan
-setenv fish_color_normal normal
-setenv fish_color_operator cyan
-setenv fish_color_param 00afff
-setenv fish_color_quote 8700ff
-setenv fish_color_redirection normal
-setenv fish_color_search_match \x2d\x2dbackground\x3dpurple
-setenv fish_color_selection \x2d\x2dbackground\x3dpurple
-setenv fish_color_status red
-setenv fish_color_user green
-setenv fish_color_valid_path \x2d\x2dunderline
-setenv fish_pager_color_completion normal
-setenv fish_pager_color_description 555\x1eyellow
-setenv fish_pager_color_prefix cyan
-setenv fish_pager_color_progress cyan
+set -gx fish_color_autosuggestion 8a8a8a
+set -gx fish_color_command 005fd7
+set -gx fish_color_comment ff8700
+set -gx fish_color_cwd green
+set -gx fish_color_cwd_root red
+set -gx fish_color_end 00FF00
+set -gx fish_color_error FF0000
+set -gx fish_color_escape cyan
+set -gx fish_color_history_current cyan
+set -gx fish_color_host normal
+set -gx fish_color_match cyan
+set -gx fish_color_normal normal
+set -gx fish_color_operator cyan
+set -gx fish_color_param 00afff
+set -gx fish_color_quote 8700ff
+set -gx fish_color_redirection normal
+set -gx fish_color_search_match \x2d\x2dbackground\x3dpurple
+set -gx fish_color_selection \x2d\x2dbackground\x3dpurple
+set -gx fish_color_status red
+set -gx fish_color_user green
+set -gx fish_color_valid_path \x2d\x2dunderline
+set -gx fish_pager_color_completion normal
+set -gx fish_pager_color_description 555\x1eyellow
+set -gx fish_pager_color_prefix cyan
+set -gx fish_pager_color_progress cyan
 

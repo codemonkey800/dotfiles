3041258b0737ef8b671625da2a891425fbafdc02
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-02-09 03:26:26 -0800

    removed nonexistent nvim plugin repo
    modified virtual.py script a bit, left in broken state right now

--- bin/virtual.py
+++ bin/virtual.py
@@ -5,28 +5,59 @@ import os.path as path
 
 from jinja2 import Environment, FileSystemLoader, Template
 from plumbum import cli
+from plumbum.cmd import mkdir
+
+SUPPORTED_PLATFORMS = [
+    'go',
+    'node',
+    'python',
+]
+
+TEMPLATE_DIR = path.realpath(f'{path.dirname(__file__)}/../share/virtual')
 
 class Virtual(cli.Application):
-    TEMPLATE_DIR = path.realpath(f'{path.dirname(__file__)}/../share/virtual')
+    '''A script that bootstraps virtual environments.'''
+
+    PROGNAME = 'Virtual'
     VERSION = '0.1.0'
 
-    @cli.switch(
-        names=['--template-dir'],
-        help='Set custom directory to search for templates.',
-    )
-    def template_dir(self, dir):
-        self.template_dir = dir
-
-    @cli.switch(
+    virtual_dir = cli.SwitchAttr(
         names=['-d', '--directory'],
+        argtype=str,
+        argname='directory',
         help='Set custom directory to setup the virtual environment in.',
+        default='.venv',
     )
-    def virtual_dir(self, dir):
-        self.directory = dir
 
-    def main(self):
-        self.env = Environment(loader=FileSystemLoader(self.TEMPLATE_DIR))
+    template_dir = cli.SwitchAttr(
+        names=['--template-dir'],
+        argtype=str,
+        argname='directory',
+        help='Set custom directory to search for templates.',
+        default=TEMPLATE_DIR,
+    )
+
+    def __init__(self, executable):
+        super().__init__(self)
+        self._list_platforms_flag_used = False
+
+    @cli.switch(['-l', '--list-platforms'])
+    def list_platforms(self):
+        '''List available platforms for bootstrapping.'''
+        print('Supported Platforms:')
+        for p in SUPPORTED_PLATFORMS:
+            print(f'    {p}')
+        self._list_platforms_flag_used = True
+
+    def main(self, env: cli.Set(*SUPPORTED_PLATFORMS) = 'node'):
+        if self._list_platforms_flag_used:
+            return
+
+        self.env = Environment(loader=FileSystemLoader(self.template_dir))
+
+    def _fetch(self):
+        ...
 
 if __name__ == '__main__':
-    Virtual.run()
+    Virtual()
 

80572ee775cc3b0adfc4a8625deca8a699b7d388
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-02-19 19:26:53 -0800

    Modified fish scripts to use single quotes if double quotes aren't needed

--- config/fish/functions/tree.fish
+++ config/fish/functions/tree.fish
@@ -1,6 +1,6 @@
 if exists tree
-    function tree -d "Runs the tree command with color enabled and pipes it to less for paging"
-       eval  (which tree) -faC $argv | less
+    function tree -d 'Runs the tree command with color enabled and pipes it to less for paging'
+       eval (which tree) -faC $argv | less
     end
 end
 

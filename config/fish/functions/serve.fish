68e1a412bb2a7d226a258e91804f3b01aefdfcee
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-02-19 19:26:53 -0800

    Modified fish scripts to use single quotes if double quotes aren't needed

--- config/fish/functions/serve.fish
+++ config/fish/functions/serve.fish
@@ -1,5 +1,5 @@
 function serve --description 'Starts a static HTTP server in the current dir.'
-    if test "$argv[1]" = 80
+    if test $argv[1] = 80
         sudo python3 -m http.server 80
     else
        python3 -m http.server $argv[1]

355539d04ff4b312cba2ee41e271070bb6959fff
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-02-22 01:37:41 -0800

    Added custom fzf commands for find file, cd, and cd hidden to use pt or find

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -45,6 +45,76 @@ __add_to_path $ANDROID_HOME/platform-tools
 # FZF stuff
 if type -q fzf
     set -gx FZF_TMUX 1
+
+    # set fzf find file command to use the_platinum_searcher if in git repo,
+    # otherwise use find command
+    # this includes hidden files and directories by default
+    set -gx FZF_FIND_FILE_COMMAND '
+        if git status > /dev/null ^&1
+            set -l files (
+                pt -g "" \
+                   --hidden \
+                   --global-gitignore \
+                   --ignore .git \
+                   ^ /dev/null
+            )
+            for f in $files
+                echo $f
+                dirname $f
+            end | sort -fru
+        else
+            command find -L . \
+                \\( -path "*/\\.git*" -o -fstype dev -o -fstype proc \\) -prune \
+                -o -type f -print \
+                -o -type d -print \
+                -o -type l -print ^ /dev/null \
+                | sed 1d | cut -b3-
+        end
+    '
+
+    # set fzf cd command to use the_platinum_searcher if in git repo,
+    # otherwise use find command
+    set -gx FZF_CD_COMMAND '
+        if git status > /dev/null ^&1
+            set -l files (
+                pt -g "" \
+                   --global-gitignore \
+                   --ignore .git \
+                   ^ /dev/null
+            )
+            for f in $files
+                set -l dir (dirname $f)
+                test "$dir" != "."; and echo $dir
+            end | sort -fru
+        else
+            command find -L . \
+                \\( -path "*/\\.*" -o -fstype dev -o -fstype proc \\) -prune \
+                -o -type d -print ^ /dev/null \
+                | sed 1d | cut -b3-
+        end
+    '
+
+    # set fzf cd with hidden command to use the platinum_searcher if in git repo,
+    # otherwise use find command
+    set -gx FZF_CD_WITH_HIDDEN_COMMAND '
+        if git status > /dev/null ^&1
+            set -l files (
+                pt -fg "" \
+                   --hidden \
+                   --global-gitignore \
+                   --ignore .git \
+                   ^ /dev/null
+            )
+            for f in $files
+                dirname $f
+            end | sort -fru
+        else
+            command find -L . \
+                \\( -path "*/\\.git*" -o -fstype dev -o -fstype proc \\) -prune \
+                -o -type d -print ^ /dev/null \
+                | sed 1d |  cut -b3-
+        end
+    '
 end
 
 # color settings

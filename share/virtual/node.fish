52ab7bfc5f23dd93c3a50e986fa5aacc78afc23e
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-03-04 05:16:24 -0800

    Modified virtual cache file name
    Added function that add's the node_modules/.bin dir to the path

--- share/virtual/node.fish
+++ share/virtual/node.fish
@@ -1,17 +1,27 @@
 # this file is part of the virtual script
 # do not run directly except inside virtual environment
 
+function add-node-module
+    set -l dir (yarn bin)
+    if test -d $dir
+        set -gx PATH $PATH $dir
+    else
+        echo "Nothing added for $PWD"
+    end
+end
+
 function deactivate
     set -e VIRTUAL_ENV
     if set -q OLD_PATH
         set -gx PATH $OLD_PATH
         set -e OLD_PATH
     end
-    functions -e deactivate
+    functions -e deactivate add-node-module
 end
 
 set -gx VIRTUAL_ENV $PWD/{venv_dir}
 set -gx OLD_PATH $PATH
 set -gx PATH $PATH $PWD/{venv_dir}/bin
 
-set -gx PATH $PATH (yarn bin) > /dev/null ^ /dev/null
+add-node-module
+

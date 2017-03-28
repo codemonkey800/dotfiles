1c01e273cd4fef789bf0ee2c067b112b8cf6911c
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-03-28 06:06:57 -0700

    :wrench: Updated fish_greeting to include fortunes from more files

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,7 +1,7 @@
 function fish_greeting -d 'Displays a fortune in a cowsay cow and pipes it to lolcat for color.'
     set cows (cowsay -l | sed 1d | string split ' ')
     set cow $cows[(random 1 (count $cows))]
-    fortune 50% myfortunes 30% off 20% ascii-art \
+    fortune -n long computers linux off myfortunes \
         | cowsay -nf $cow \
         | lolcat
     echo

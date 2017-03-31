9f510bbdeb42ff38e2fc7fc10f777a51b607c9af
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-03-31 14:19:51 -0700

    :wrench: Removed nuclide keymaps, added custom user command to dismiss notifications

--- config/atom/init.js
+++ config/atom/init.js
@@ -1,1 +1,8 @@
 /** @babel */
+
+atom.commands.add('atom-workspace', 'user:dismiss-notifications', () => {
+  atom.notifications.getNotifications().forEach(
+      notification => notification.dismiss(),
+  );
+  atom.notifications.clear();
+});

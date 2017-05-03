4e1d8ede1627ad794ee6629509f5c6b1d7e76179
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-05-02 21:32:18 -0700

    :wrench: Replaced the_platinum_sercher with the_silver_sercher, updated fish vars and nvim config to use ag instead of pt

--- config/fish/variables.fish
+++ config/fish/variables.fish
@@ -27,15 +27,13 @@ set -gx PATH $PATH $DOTFILES/bin
 if type -q fzf
     set -gx FZF_TMUX 1
 
-    # set fzf find file command to use the_platinum_searcher if in git repo,
-    # otherwise use find command
+    # set fzf find file command to use the_silver_searcher
     # this includes hidden files and directories by default
     set -gx FZF_FIND_FILE_COMMAND '
         if git status > /dev/null ^&1
             set -l files (
-                pt -g "" \
+                ag -g "" \
                    --hidden \
-                   --global-gitignore \
                    --ignore .git \
                    ^ /dev/null
             )
@@ -43,58 +41,29 @@ if type -q fzf
                 echo $f
                 dirname $f
             end | sort -fru
-        else
-            command find -L . \
-                \\( -path "*/\\.git*" -o -fstype dev -o -fstype proc \\) -prune \
-                -o -type f -print \
-                -o -type d -print \
-                -o -type l -print ^ /dev/null \
-                | sed 1d | cut -b3-
-        end
     '
 
-    # set fzf cd command to use the_platinum_searcher if in git repo,
-    # otherwise use find command
+    # set fzf cd command to use the_silver_searcher
     set -gx FZF_CD_COMMAND '
-        if git status > /dev/null ^&1
-            set -l files (
-                pt -g "" \
-                   --global-gitignore \
-                   --ignore .git \
-                   ^ /dev/null
-            )
-            for f in $files
-                set -l dir (dirname $f)
-                test "$dir" != "."; and echo $dir
-            end | sort -fru
-        else
-            command find -L . \
-                \\( -path "*/\\.*" -o -fstype dev -o -fstype proc \\) -prune \
-                -o -type d -print ^ /dev/null \
-                | sed 1d | cut -b3-
-        end
+        set -l files (ag -g "" --ignore .git ^ /dev/null)
+        for f in $files
+            set -l dir (dirname $f)
+            test "$dir" != "."; and echo $dir
+        end | sort -fru
     '
 
     # set fzf cd with hidden command to use the platinum_searcher if in git repo,
     # otherwise use find command
     set -gx FZF_CD_WITH_HIDDEN_COMMAND '
-        if git status > /dev/null ^&1
-            set -l files (
-                pt -fg "" \
-                   --hidden \
-                   --global-gitignore \
-                   --ignore .git \
-                   ^ /dev/null
-            )
-            for f in $files
-                dirname $f
-            end | sort -fru
-        else
-            command find -L . \
-                \\( -path "*/\\.git*" -o -fstype dev -o -fstype proc \\) -prune \
-                -o -type d -print ^ /dev/null \
-                | sed 1d |  cut -b3-
-        end
+        set -l files (
+            ag -fg "" \
+               --hidden \
+               --ignore .git \
+               ^ /dev/null
+        )
+        for f in $files
+            dirname $f
+        end | sort -fru
     '
 end
 

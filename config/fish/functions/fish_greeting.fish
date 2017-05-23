5d107db400f1f5188b108a64d52d8baf2f91ce09
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-05-22 18:07:18 -0700

    :wrench: Made cleanup a fish function, added random-cow function for random cow files

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,8 +1,6 @@
 function fish_greeting -d 'Displays a fortune in a cowsay cow and pipes it to lolcat for color.'
-  set cows (cowsay -l | sed 1d | string split ' ')
-  set cow $cows[(random 1 (count $cows))]
   fortune -n long computers linux off myfortunes \
-    | cowsay -nf $cow \
+    | cowsay -nf (random-cow) \
     | lolcat
   echo
 end

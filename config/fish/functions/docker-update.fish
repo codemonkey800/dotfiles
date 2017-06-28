b246267e746486db4837ef5b6b1ec6c4e54e8822
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-06-27 23:16:35 -0700

    :wrench: Updated docker-* fish functions and git url rewrites

--- config/fish/functions/docker-update.fish
+++ config/fish/functions/docker-update.fish
@@ -1,8 +1,6 @@
 function docker-update -d 'Updates all docker images.'
-  for image in (command docker images | pawk 'f[0]' | tail -n +2)
-    command docker pull $image
+  for image in (docker images --format '{{ .Repository }}:{{ .Tag }}')
+    docker pull $image
   end
-
-  clear-functions __docker_update
 end
 

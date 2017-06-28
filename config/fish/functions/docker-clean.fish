415b2ce00fd587857e3b5edcfc74d0e13028dcf1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-06-27 23:21:47 -0700

    :wrench: Added echo statements for docker-clean function

--- config/fish/functions/docker-clean.fish
+++ config/fish/functions/docker-clean.fish
@@ -5,17 +5,20 @@ function docker-clean -d 'Cleans up non-running contains, <none> images, and vol
 
   set volume_count (count $volumes)
   if test $volume_count -gt 0
-    docker volume rm $volumes
+    docker volume rm $volumes > /dev/null ^&1
   end
+  echo "Removed $volume_count volumes."
 
   set container_count (count $containers)
   if test $container_count -gt 0
-    docker rm $containers
+    docker rm $containers > /dev/null ^&1
   end
+  echo "Removed $countainer_count containers."
 
   set image_count (count $images)
   if test $image_count -gt 0
-    docker rmi $images
+    docker rmi $images > /dev/null ^&1
   end
+  echo "Removed $image_count images."
 end
 

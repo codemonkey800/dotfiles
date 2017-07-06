21e19b93c3b65b9172542266aa644e80fbc3a5a1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-07-06 16:55:13 -0700

    :wrench: Fixed comments in update script

--- config/fish/functions/update.fish
+++ config/fish/functions/update.fish
@@ -1,11 +1,11 @@
 function update -d 'Runs various updates on my Arch system.'
-# Run only on Arch Linux
+  # Run only on Arch Linux
   if not lsb_release -d | grep -q 'Arch Linux'
     echo 'This script can only be run on Arch Linux!'
     exit -1
   end
 
-# Update pacman db
+  # Update pacman db
   sudo reflector \
     --verbose \
     --threads (nproc) \
@@ -14,21 +14,21 @@ function update -d 'Runs various updates on my Arch system.'
     --country US \
     --fastest 10
 
-# Update pkgfile definitions
+  # Update pkgfile definitions
   sudo pkgfile -u
 
-# Update fisher packages and re-link fish functions from $DOTFILES
+  # Update fisher packages and re-link fish functions from $DOTFILES
   fisher up
   and ln -svf $DOTFILES/config/fish/functions/* ~/etc/fish/functions/
 
-# Update atom packages
+  # Update atom packages
   for x in apm{,-beta}
     if type -q $x
       eval $x upgrade --no-confirm
     end
   end
 
-# Update pacman packages from official and aur
+  # Update pacman packages from official and aur
   yaourt -Syua --force --noconfirm
 end
 

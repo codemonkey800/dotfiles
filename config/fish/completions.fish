5b481a567ebbe2bbe14649c44d6aea2238296c6d
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-12-06 14:46:32 -0800

    :wrench: Fixed completions for dotfiles command

--- config/fish/completions.fish
+++ config/fish/completions.fish
@@ -10,19 +10,24 @@ complete -c r -w rm
 complete -c re -w nvim
 
 # Function completions
-complete -c dotfiles -x -d 'Open directory or open file for editing' -a (
-  set files (ag -g '')
-  set dirs
+function __dotfiles_list_files
+  set files (ag -g '' $DOTFILES)
 
   for file in $files
-    set -l dir (dirname $file)
-    if test $dir != .
-      set dirs $dirs $dir
-    end
+   set -l dir (dirname $file)
+   if test $dir != $DOTFILES
+     set files $files $dir
+   end
   end
 
-  echo $files $dirs | sort -u
-)
+  for i in (seq (count $files))
+    set files[$i] (echo $files[$i] | string replace $DOTFILES '')
+  end
+
+  printf (echo $files | string replace -a ' ' '\n') | sort -u
+end
+
+complete -c dotfiles -x -d 'Open directory or open file for editing' -a '(__dotfiles_list_files)'
 
 complete -c node-deps -x -d 'Lists only development dependencies.' -a dev
 complete -c node-deps -x -d 'Lists only production dependencies.' -a prod

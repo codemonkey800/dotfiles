2aa51dd65ec6899fc27252b968c525055ee72141
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2017-12-06 17:30:41 -0800

    :wrench: Updated gitignore function to use fzf with 0 args

--- config/fish/functions/gitignore.fish
+++ config/fish/functions/gitignore.fish
@@ -11,7 +11,7 @@ function gitignore -d "Retrieves gitignore files from gitignore.io"
     echo 'Notes:'
     echo '  You can add multiple templates by using spaces or commas to'
     echo '  separate them. The comma format is the default used by'
-    echo '  gitignore.io.'
+    echo '  gitignore.io. If you omit any arguments, then an fzf window will pop up.'
     echo
     echo 'Example:'
     echo '  Fetch gitignore for node'
@@ -43,14 +43,31 @@ function gitignore -d "Retrieves gitignore files from gitignore.io"
     end
   end
 
+  function __gitignore_fzf
+    set endpoint 'https://gitignore.io/api'
+    set selected (
+      curl -sL "$endpoint/list" \
+        | tr ',' '\n' \
+        | sort -u \
+        | fzf-tmux -m
+    )
+
+    if test -z $selected
+      echo 'No templates selected.'
+      return
+    end
+
+    set selected (echo $selected | tr ' ' ',')
+    curl -sL "$endpoint/$selected"
+  end
+
   set argc (count $argv)
   set exit_code 0
   set current_ifs "$IFS"
 
   if test $argc -eq 0
-    __gitignore_help
-    return
-  else if test $argc -eq 1
+    __gitignore_fzf
+  else
     switch "$argv[1]"
       case 'list'
         __gitignore_fetch list | string split ','
@@ -61,24 +78,16 @@ function gitignore -d "Retrieves gitignore files from gitignore.io"
         # is to set the IFS variable to an empty string. Thank you to
         # glenn jackman for the solution to this problem:
         # https://stackoverflow.com/a/29515237
+        set -l endpoint (string join ',' $argv)
+
         set IFS ''
-        set -l response (__gitignore_fetch "$argv[1]")
+        set -l response (__gitignore_fetch "$endpoint")
         set IFS "$current_ifs"
 
         __gitignore_check_response "$response"
         and echo "$response"
         or set exit_code -1
-    end
-  else
-    set -l endpoint (string join ',' $argv)
-
-    set IFS ''
-    set -l response (__gitignore_fetch "$endpoint")
-    set IFS "$current_ifs"
-
-    __gitignore_check_response "$response"
-    and echo "$response"
-    or set exit_code -1
+      end
   end
 
   clear-functions '__gitignore'

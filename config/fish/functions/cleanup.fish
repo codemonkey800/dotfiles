8d490ac8205a773bb384528bd935e0828435a61e
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-02-20 12:23:30 -0800

    :wrench: Removed fish code to delete /tmp files

--- config/fish/functions/cleanup.fish
+++ config/fish/functions/cleanup.fish
@@ -5,13 +5,6 @@ function cleanup -d 'Runs several cleanup utilities on my computer.'
     sudo pacman -Rscun --noconfirm $packages
   end
 
-  set tmpfiles /tmp/{flow,fzf,npm,nvim,yaourt}*
-  set tmpfiles_length (count $tmpfile)
-  if test $tmpfiles_length -gt 0
-    echo "Removing $tmpfiles_length temporary files!"
-    sudo rm -rf $tmpfiles
-  end
-
   rmshit -vy > /dev/null ^&1
 
   echo -n 'Done Cleaning!' | cowsay -f (random-cow) | lolcat

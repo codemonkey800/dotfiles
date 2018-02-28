9395d751b6a617c101eb3bf7c8ecc10cf70ad994
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-02-28 15:51:26 -0800

    :wrench: Refactored yarn.fish script and removed prefix flag for __yarn_global.fish

--- config/fish/functions/__yarn_global.fish
+++ config/fish/functions/__yarn_global.fish
@@ -1,7 +1,4 @@
 function __yarn_global -d 'Wrapper over yarn global subcommand.'
-  set yarn_dir $PWD/.yarn
-  set cmd "command yarn global --prefix=$yarn_dir"
-
   if test "$argv[1]" = 'ls'
     set -e argv[1]
     set cmd "$cmd list"

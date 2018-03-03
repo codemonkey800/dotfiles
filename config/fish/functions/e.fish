ade7176a787ae5e9492144d45b5004594bb3a328
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-03-03 05:09:38 -0800

    :wrench: Fixed iteration issue in e.fish

--- config/fish/functions/e.fish
+++ config/fish/functions/e.fish
@@ -1,7 +1,7 @@
 function e -d 'Edit files with Neovim'
   set cmd 'nvim'
 
-  for i in (count $argv)
+  for i in (seq (count $argv))
     # I rarely use the  -r flag for nvim, so it's perfect.
     if contains -- "$argv[$i]" '-r' '--remote'
       set cmd 'nvr --remote-silent'

0f3acfedb4ee758e72f7116d543d048d9a175e48
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-03-03 02:24:08 -0800

    :wrench: Added neovim-remote and moved e alias to function

---
+++ config/fish/functions/e.fish
@@ -0,0 +1,14 @@
+function e -d 'Edit files with Neovim'
+  set cmd 'nvim'
+
+  for i in (count $argv)
+    # I rarely use the  -r flag for nvim, so it's perfect.
+    if contains -- "$argv[$i]" '-r' '--remote'
+      set cmd 'nvr --remote-silent'
+      set -e argv[$i]
+    end
+  end
+
+  eval "$cmd $argv"
+end
+

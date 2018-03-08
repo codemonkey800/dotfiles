e228a2fa766f57c54a62cb769f8d3b6c881214c2
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-03-08 15:27:47 -0800

    :wrench: Created single executable file for running python scripts

---
+++ bin/py/run.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+set name (basename (status -f))
+set root $DOTFILES/bin/py
+set script (
+  test -f $root/$name.py
+  and echo $root/$name.py
+  or echo $root/$name/main.py
+)
+
+source $root/env.fish
+echo python $script $argv
+
+deactivate
+

b4c80cb3e585c481e39ba0c6ea93cb6ae90ebfb4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-03-27 23:18:51 -0700

    :wrench: Added directory arg to gr function

--- config/fish/functions/gr.fish
+++ config/fish/functions/gr.fish
@@ -1,14 +1,21 @@
 function gr -d 'Convenience function for changing or printing the git root dir.'
-  if set git_dir (git rev-parse --show-toplevel)
-    # Prints out root dir if in command substitution
-    if status -c
-      echo "$git_dir"
-    else
-      cd "$git_dir"
-    end
-  else
+  if not set git_dir (git rev-parse --show-toplevel)
     echo 'Not in git directory!'
     return -1
   end
+
+  # Prints out root dir if in command substitution
+  if status -c
+    echo "$git_dir"
+    return
+  end
+
+  set path "$git_dir/$argv[1]"
+  if test ! -d "$path"
+    echo "$path does not exist!"
+    return -1
+  end
+
+  cd "$path"
 end
 

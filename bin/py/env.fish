503d6b07bdff32f29f41d3b58e86034213d0f202
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-07-26 14:38:12 -0700

    :wrench: Updated pip requirements and removed flag from bin/py/env.fish script

--- bin/py/env.fish
+++ bin/py/env.fish
@@ -3,7 +3,7 @@ set activate_file $venv/bin/activate.fish
 
 if not test -f $activate_file
   echo 'Environment not available. Creating one.'
-  python3 -m venv --system-site-packages $venv
+  python3 -m venv $venv
   source $activate_file
   pip install -r $DOTFILES/requirements.txt
   clear

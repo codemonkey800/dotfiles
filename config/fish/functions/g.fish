f5c03a73648d75f443b0a62f47bbcf687073b5a1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-07-31 16:15:31 -0700

    :wrench: Updated g wrapper to not modify gitconfig on linux

--- config/fish/functions/g.fish
+++ config/fish/functions/g.fish
@@ -1,25 +1,24 @@
-function g -d 'Git wrapper with added sugar on top.'
-  set git '/usr/bin/git'
-  set is_macos false
-
-  if test (uname) = 'Darwin'
-    set git '/usr/local/bin/git'
-    set is_macos true
-    eval "$git config --global gpg.program /usr/local/MacGPG2/bin/gpg2"
-  else
-    eval "$git config --global gpg.program /usr/bin/gpg2"
+function g -w git -d 'Git wrapper with added sugar on top.'
+  # If on Linux, just use normal git command and git config.
+  if test (uname) = 'Linux'
+    git $argv
+    return
   end
 
+  # Assume from here we're on macOS.
+  alias __g_git '/usr/local/bin/git'
+
+  # Switch to macOS gpg2
+  __g_git config --global 'gpg.program' '/usr/local/MacGPG2/bin/gpg2'
+
+  # Dynamically switch between Sumo Logic email and personal email.
   if string match -qr '.*projects/sumo.*' $PWD
-    eval "$git config --global user.email jasuncion@sumologic.com"
+    __g_git config --global 'user.email' 'jasuncion@sumologic.com'
   else
-    eval "$git config --global user.email jeremyasuncion808@gmail.com"
+    __g_git config --global 'user.email' 'jeremyasuncion808@gmail.com'
   end
 
-  if eval $is_macos
-    /usr/local/bin/git $argv
-  else
-    /usr/bin/git $argv
-  end
+  __g_git $argv
+  clear-functions __g
 end
 

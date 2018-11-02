19ec6dcb0ce9a43c4897c714e8389a063388c7fe
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2018-11-02 11:41:46 -0700

    :wrench: Added macos alias for chrome

--- config/fish/core/02-aliases.fish
+++ config/fish/core/02-aliases.fish
@@ -34,6 +34,7 @@ alias yas 'ya -S'
 
 # macOS aliases and overrides
 if test (uname) = 'Darwin'
+  alias chrome '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
   alias code '/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
   alias code-insiders '/Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code'
   alias git '/usr/local/bin/git'

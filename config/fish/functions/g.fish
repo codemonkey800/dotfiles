6ef2a653fa6575d3b6539547fc49e6082d4dc0b5
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2019-01-02 14:04:26 -0800

    :wrench: Refactored g.fish and njs.fish, removed clear-functions.fish

--- config/fish/functions/g.fish
+++ config/fish/functions/g.fish
@@ -6,19 +6,18 @@ function g -w git -d 'Git wrapper with added sugar on top.'
   end
 
   # Assume from here we're on macOS.
-  alias __g_git '/usr/local/bin/git'
+  set git '/usr/local/bin/git'
 
   # Switch to macOS gpg2
-  __g_git config --global 'gpg.program' '/usr/local/MacGPG2/bin/gpg2'
+  $git config --global 'gpg.program' '/usr/local/MacGPG2/bin/gpg2'
 
   # Dynamically switch between Sumo Logic email and personal email.
   if string match -qr '.*projects/sumo.*' $PWD
-    __g_git config --global 'user.email' 'jasuncion@sumologic.com'
+    $git config --global 'user.email' 'jasuncion@sumologic.com'
   else
-    __g_git config --global 'user.email' 'jeremyasuncion808@gmail.com'
+    $git config --global 'user.email' 'jeremyasuncion808@gmail.com'
   end
 
-  __g_git $argv
-  clear-functions __g
+  $git $argv
 end
 
--- config/fish/functions/njs.fish
+++ config/fish/functions/njs.fish
@@ -1,10 +1,10 @@
-function njs -d 'Simplescript for installing node.js and yarn'
-  set version $argv[1]
+function njs -d 'Simple script for installing node.js and yarn'
+  set node_version $argv[1]
   set os (uname | tr '[:upper:]' '[:lower:]')
   set update_npm false
 
-  if test -e $version
-    echo 'Usage: njs <version|profile>'
+  if test -e $node_version
+    echo 'Usage: njs <node_version|profile>'
     echo
     echo 'Examples:'
     echo '  $ njs 7.9.0'
@@ -16,21 +16,24 @@ function njs -d 'Simplescript for installing node.js and yarn'
     return
   end
 
-  switch "$version"
+  switch "$node_version"
     case 'current'
-      set version (curl -sL 'https://semver.io/node/unstable')
+      set node_version (curl -sL 'https://semver.io/node/unstable')
       set update_npm true
     case 'sumo'
-      set version '8.11.4'
+      set node_version '8.11.4'
+    case '*'
+      echo "Invalid node_version: '$node_version'"
+      exit -1
   end
 
   mkdir -p ~/.nodejs
   rm -rf ~/.nodejs/{node,yarn}
 
   pushd ~/.nodejs
-    echo "Fetching node v$version"
-    set -l base "node-v$version-$os-x64"
-    wget -qO- --show-progress "https://nodejs.org/dist/v$version/$base.tar.gz" | tar -zxf -
+    echo "Fetching node v$node_version"
+    set -l base "node-v$node_version-$os-x64"
+    wget -qO- --show-progress "https://nodejs.org/dist/v$node_version/$base.tar.gz" | tar -zxf -
     mv $base node
 
     echo 'Fetching latest yarn'

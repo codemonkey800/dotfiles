86f01ba5b136cf6cd21c79d811edce795b70c16e
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2019-01-02 12:43:21 -0800

    :wrench: Removed run.fish, reduced complexity of e.fish and l.fish

--- config/fish/functions/e.fish
+++ config/fish/functions/e.fish
@@ -1,14 +1,4 @@
 function e -d 'Edit files with Neovim'
-  set cmd 'nvim'
-
-  for i in (seq (count $argv))
-    # I rarely use the  -r flag for nvim, so it's perfect.
-    if contains -- "$argv[$i]" '-r' '--remote'
-      set cmd 'nvr --remote-silent'
-      set -e argv[$i]
-    end
-  end
-
-  eval "$cmd $argv"
+  nvim $argv
 end
 
--- config/fish/functions/l.fish
+++ config/fish/functions/l.fish
@@ -1,59 +1,10 @@
 function l -w ls -d 'Lists all files in a directory or reads a file'
-  set pager false
-  set args
-  set files
+  set cmd 'ls -CAF'
 
-  for i in (seq (count $argv))
-    set -l arg $argv[$i]
-    switch "$arg"
-      case --pager
-        set pager true
-      case '--*' '-*'
-        set args $args $arg
-      case '*'
-        set files $files $arg
-    end
+  if test (uname) = 'Linux'
+    set cmd "$cmd --color=auto"
   end
 
-  if test (count $files) -eq 1; and test -f "$files[1]"
-    if test -f "$files[1]"
-      if eval "$pager"
-        eval "less -N $files[1]"
-      else
-        # pipe file through syntax highlighter
-        # depending on syntax
-        switch (echo "$files[1]" | pawk -F'.' 'f[-1]')
-          case 'json'
-            jq '.' "$files[1]"
-          case '*'
-            pygmentize -g "$files[1]"
-        end
-      end
-    else
-      echo "'$files[1]' doest not exist!"
-      return -1
-    end
-  else
-    for file in $files
-      if not test -e "$file"
-        echo "'$file' does not exist!"
-        return -1
-      end
-    end
-
-    set cmd 'ls -CAF'
-
-    if test (uname) = 'Linux'
-      set cmd "$cmd --color=auto"
-    end
-
-    set cmd "$cmd $args $files"
-
-    if eval $pager
-      set cmd "$cmd | $PAGER"
-    end
-
-    eval $cmd
-  end
+  eval "$cmd $argv"
 end
 

8f982d5973154e09ff9f5f33060a65ae5b15fe68
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2020-04-15 15:32:01 -0700

    update fish and vscode configs

--- config/fish/functions/cleanup.fish
+++ config/fish/functions/cleanup.fish
@@ -1,11 +1,11 @@
 function cleanup -d 'Runs several cleanup utilities on my computer.'
-  echo 'y' | sudo pacman -Scc > /dev/null ^&1
+  echo 'y' | sudo pacman -Scc &> /dev/null
 
   if set packages (pacman -Qqdtt)
     sudo pacman -Rscun --noconfirm $packages
   end
 
-  rmshit -vy > /dev/null ^&1
+  rmshit -vy &> /dev/null
 
   echo -n 'Done Cleaning!' | cowsay -f (random-cow) | lolcat
 end

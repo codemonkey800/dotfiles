eb2acd1f7c64e23d466cf3c4e21c77506740e7da
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2020-04-24 22:00:43 -0700

    add deploy to PATH

--- config/fish/core/04-events.fish
+++ config/fish/core/04-events.fish
@@ -1,6 +1,9 @@
 function __add_pm_tools_dir --on-variable PWD
   if string match -qe ~/projects/postmates $PWD
-    set PATH $PATH ~/projects/postmates/tools/bin
+    set PATH \
+      $PATH \
+      ~/projects/postmates/tools/bin \
+      ~/projects/postmates/skel/src/cloud/deploy
   end
 end
 

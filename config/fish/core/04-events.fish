729fa3633bc0d52b98a2a63229150ae1894cd69d
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2020-05-04 03:03:29 -0700

    added more directories to postmates PATH event

--- config/fish/core/04-events.fish
+++ config/fish/core/04-events.fish
@@ -2,8 +2,11 @@ function __add_pm_tools_dir --on-variable PWD
   if string match -qe ~/projects/postmates $PWD
     set PATH \
       $PATH \
-      ~/projects/postmates/tools/bin \
-      ~/projects/postmates/skel/src/cloud/deploy
+      ~/projects/postmates/{ \
+        tools/bin, \
+        skel/src/cloud/deploy, \
+        phantom-operator, \
+      }
   end
 end
 

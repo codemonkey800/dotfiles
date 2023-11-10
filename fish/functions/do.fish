8b0a7510872c43c908046e813b3826ff89aea13a
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-11-10 13:49:22 -0800

    uncomment do script

--- fish/functions/do.fish
+++ fish/functions/do.fish
@@ -2,9 +2,9 @@ function do -w docker -d 'Wrapper for docker command.'
   set command "$argv[1]"
 
   if type -q "__do_$command"
-    echo eval "__do_$command $argv[2..-1]"
+    eval "__do_$command $argv[2..-1]"
   else
-    echo docker $argv
+    docker $argv
   end
 end
 

6045b304c658079f0d1c7947ca3982889faf3bc8
Jeremy <jeremyasuncion808@gmail.com>
2024-12-26 13:47:42 -0800

    remove references to ME

--- fish/functions/g.fish
+++ fish/functions/g.fish
@@ -7,10 +7,6 @@ function g -d 'Wrapper over git' -w git
     set git '/opt/homebrew/bin/git'
   end
 
-  if string match "$HOME/dev/me/*" $PWD > /dev/null
-    set email 'jeremy@magiceden.io'
-  end
-
   if test ($git config --global user.email) != "$email"
     $git config --global user.email $email
   end
